8ce2ea7e1e1f0e526e337bf9fa3996a9c1929f36
UmarGit <umarsheikh303@gmail.com>
2021-02-04 17:35:58 +0500

    Update Automation

--- index.js
+++ index.js
@@ -17,87 +17,105 @@ const main = () => {
 
     fs.readdirSync(base + "/" + chapter).forEach(list => {
 
-      if(!list.includes('.txt')){
+      if (!list.includes('.txt')) {
         listformat = "  - <span>" + list.slice(2, list.length).replace(/-/g, " ") + "</span>" + "\n"
         SideBar_Template += listformat
-  
+
         fs.readdirSync(base + "/" + chapter + "/" + list).forEach(file => {
-  
-          
+
+
           if (file.endsWith(".md") && !file.includes('.txt')) {
             fileformat = "    - [" + file.replace(/-/g, " ").replace(/.md/g, "") + "](/chapters/" + chapter + "/" + list + "/" + file + ")" + "\n"
             SideBar_Template += fileformat
             let CommitFile = fs.readFileSync(base + "/" + chapter + "/" + list + "/" + file).toString('utf-8')
-  
-            if (CommitFile.length > 0) {
-              const index = CommitFile.search("This page was last updated");
-    
-              const start = index + 30
-    
-              const end = index + 45
-    
-              var options = {
-                host: 'api.github.com',
-                path: "/repos/UmarGit/docs/commits?path=docs/chapters" + "/" + chapter + "/" + list + "/" + file + "&page=1&per_page=1&sha=ed30a9473fa71a6aec9f2618db3989ea55cd7da5",
-                method: 'GET',
-                headers: { 'user-agent': 'node.js' }
-              };
-    
-              https.get(options, (resp) => {
-                let data = '';
-    
-                resp.on('data', (chunk) => {
-                  data += chunk;
-                });
-    
-                resp.on('end', () => {
-                  var date = new Date(JSON.parse(data)[0]['commit']['committer']['date']).toDateString()
-                  let UpdateCommitDate = CommitFile.slice(start, end)
-                  CommitFile = CommitFile.replace(UpdateCommitDate, date);
-                  fs.writeFileSync(base + "/" + chapter + "/" + list + "/" + file, CommitFile)
-                });
-    
-              }).on("error", (err) => {
-                console.log("Error: " + err.message);
+
+            var request = {
+              host: 'api.github.com',
+              path: "/repos/UmarGit/docs/branches/gh-pages",
+              method: 'GET',
+              headers: { 'user-agent': 'node.js' }
+            };
+
+            https.get(request, (res) => {
+              let response = '';
+
+              res.on('data', (chunk) => {
+                response += chunk;
               });
-            }
-    
-            let README = fs.readFileSync("./docs/README.md").toString('utf-8')
-    
-            if(README.length > 0){
-              const index = README.search("This page was last updated");
-    
-              const start = index + 30
-    
-              const end = index + 45
-    
-              var options = {
-                host: 'api.github.com',
-                path: "/repos/UmarGit/docs/commits?path=README.md&page=1&per_page=1&sha=ed30a9473fa71a6aec9f2618db3989ea55cd7da5",
-                method: 'GET',
-                headers: { 'user-agent': 'node.js' }
-              };
-      
-              https.get(options, (resp) => {
-                let data = '';
-      
-                resp.on('data', (chunk) => {
-                  data += chunk;
-                });
-      
-                resp.on('end', () => {
-                  var date = new Date(JSON.parse(data)[0]['commit']['committer']['date']).toDateString()
-                  let UpdateCommitDate = README.slice(start, end)
-                  README = README.replace(UpdateCommitDate, date);
-                  fs.writeFileSync("./docs/README.md", README)
-                });
-      
-              }).on("error", (err) => {
-                console.log("Error: " + err.message);
-              });
-            }
+
+              res.on('end', () => {
+                var sha = JSON.parse(response)['commit']['sha']
+                if (CommitFile.length > 0) {
+                  const index = CommitFile.search("This page was last updated");
+
+                  const start = index + 30
+
+                  const end = index + 45
+
+                  var options = {
+                    host: 'api.github.com',
+                    path: "/repos/UmarGit/docs/commits?path=docs/chapters" + "/" + chapter + "/" + list + "/" + file + "&page=1&per_page=1&sha=" + sha + "",
+                    method: 'GET',
+                    headers: { 'user-agent': 'node.js' }
+                  };
+
+                  https.get(options, (resp) => {
+                    let data = '';
+
+                    resp.on('data', (chunk) => {
+                      data += chunk;
+                    });
+
+                    resp.on('end', () => {
+                      var date = new Date(JSON.parse(data)[0]['commit']['committer']['date']).toDateString()
+                      let UpdateCommitDate = CommitFile.slice(start, end)
+                      CommitFile = CommitFile.replace(UpdateCommitDate, date);
+                      fs.writeFileSync(base + "/" + chapter + "/" + list + "/" + file, CommitFile)
+                    });
+
+                  }).on("error", (err) => {
+                    console.log("Error: " + err.message);
+                  });
+                }
+
+                let README = fs.readFileSync("./docs/README.md").toString('utf-8')
+
+                if (README.length > 0) {
+                  const index = README.search("This page was last updated");
+
+                  const start = index + 30
+
+                  const end = index + 45
+
+                  var options = {
+                    host: 'api.github.com',
+                    path: "/repos/UmarGit/docs/commits?path=README.md&page=1&per_page=1&sha=ed30a9473fa71a6aec9f2618db3989ea55cd7da5",
+                    method: 'GET',
+                    headers: { 'user-agent': 'node.js' }
+                  };
+
+                  https.get(options, (resp) => {
+                    let data = '';
+
+                    resp.on('data', (chunk) => {
+                      data += chunk;
+                    });
+
+                    resp.on('end', () => {
+                      var date = new Date(JSON.parse(data)[0]['commit']['committer']['date']).toDateString()
+                      let UpdateCommitDate = README.slice(start, end)
+                      README = README.replace(UpdateCommitDate, date);
+                      fs.writeFileSync("./docs/README.md", README)
+                    });
+
+                  }).on("error", (err) => {
+                    console.log("Error: " + err.message);
+                  });
+                }
+              })
+            })
           }
-  
+
         })
       }
 
@@ -108,4 +126,3 @@ const main = () => {
 main()
 
 fs.writeFileSync("./docs/_sidebar.md", SideBar_Template);
-
